561bc861b7b04e5a42ee3867f01f55f53e46c47a
babylink1 <babylinkbabylink@gmail.com>
2023-10-09 10:05:14 -0400

    fix rep url

--- HW04a.py
+++ HW04a.py
@@ -5,7 +5,9 @@ import json
 def get_github_repositories_and_commits(user_id):
     try:
         # API endpoint to retrieve user's repositories
-        repositories_url = f"https://api.github.com/users/{user_id}/repos"
+        # repositories_url = f"https://api.github.com/users/{user_id}/repos"
+        repositories_url = "https://api.github.com/users/{}/repos".format(
+            user_id)
 
         # Send a GET request to GitHub API
         response = requests.get(repositories_url)
